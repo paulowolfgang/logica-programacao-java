677c5abf5e4d1af6f71faefd578b57c6eedb254d
paulo <paulovictor.ps.belem@gmail.com>
2022-10-27 18:56:28 -0300

    :sparkles: feat: Variável e constante

---
+++ src/Program_05.java
@@ -0,0 +1,13 @@
+
+public class Program_05 {
+
+	public static void main(String[] args) {
+		final int y = 10;
+		int x;
+		
+		x = y;
+		
+		System.out.println("Valor de Y: " + y);
+		System.out.println("Valor de X: " + x);
+	}
+}
