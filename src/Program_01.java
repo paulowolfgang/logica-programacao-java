2f4951a7cef54d6e9f55bc1b3b9862ac89a81e03
paulo <paulovictor.ps.belem@gmail.com>
2022-10-27 14:36:16 -0300

    :tada: Hello Java!

---
+++ src/Program_01.java
@@ -0,0 +1,11 @@
+/*
+ * Developer: Paulo Victor Silva
+ * Date: 27/10/2022
+ * */
+
+public class Program_01 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello Java!");
+	}
+}
