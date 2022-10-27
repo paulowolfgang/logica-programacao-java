928f9d9a7170c7acfbe99524229024b18bc26edf
paulo <paulovictor.ps.belem@gmail.com>
2022-10-27 18:48:51 -0300

    :sparkles: feat: Múltiplas impressões no console

---
+++ src/Programa_04.java
@@ -0,0 +1,9 @@
+
+public class Programa_04 {
+
+	public static void main(String[] args) {
+		System.out.println("Nome: Fulano de Tal");
+		System.out.println("Endereço: Rua da Paz, 21");
+		System.out.println("Telefone: (91) 9.9999-9999");
+	}
+}
