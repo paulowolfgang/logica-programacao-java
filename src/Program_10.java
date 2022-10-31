d9dcd77f8fe3189a56a9f42facc4da1f5e52d225
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-10-30 23:02:54 -0300

    :sparkles: feat: Exercício com tipo primitivo (double)

---
+++ src/Program_10.java
@@ -0,0 +1,12 @@
+public class Program_10 {
+    public static void main(String args[]){
+    
+        double distancia = 400;
+        double consumo = 16;
+        double taxaConsumo;
+        
+        taxaConsumo = distancia/consumo;
+        
+        System.out.println("Taxa de consumo (Km/L): " + taxaConsumo);
+    }
+}
