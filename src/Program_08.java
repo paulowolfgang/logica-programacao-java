62506749aaf4c9a7415edd04c8a35bf1a039daa5
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-10-30 22:48:57 -0300

    :sparkles: feat: Formatação com sequências de escape

---
+++ src/Program_08.java
@@ -0,0 +1,8 @@
+public class Program_08 {
+    public static void main (String args[]){
+    
+        System.out.println("\t\t1° BI\t 2°BI\t Média");
+        System.out.println("LTP\t\t 9,0\t 8,0\t 8,5");
+        System.out.println("Algoritmos\t 7,0\t 6,0\t 6,5");
+    }
+}
