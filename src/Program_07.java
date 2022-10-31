213232803604df4318a04af6a4445548876e24a3
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-10-30 22:39:59 -0300

    :sparkles: feat: Sequências de escape

---
+++ src/Program_07.java
@@ -0,0 +1,8 @@
+public class Program_07 {
+    public static void main (String args[]){
+    
+        System.out.println("Nome: Fulano da Silva \n"
+                + "Endereço: Rua da Paz, 21 \n"
+                + "Telefone: (91) 9.9999-9999");
+    }
+}
