56ec4a34c9afb3d1ce5f3ba73cbe63f2ce99b8a0
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-10-30 22:30:22 -0300

    :sparkles: feat: Tipo primitivo (byte)

---
+++ src/Program_06.java
@@ -0,0 +1,10 @@
+
+public class Program_06 {
+    public static void main (String args[]){
+    
+        byte x;
+        x = 127;
+        
+        System.out.println("Valor de X: " + x);
+    }
+}
