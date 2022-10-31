8c80d051ef1526ef283c88e35bfcfe1ccc5a3df2
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-10-30 22:53:47 -0300

    :sparkles: feat: Tipo primitivo (double)

---
+++ src/Program_09.java
@@ -0,0 +1,13 @@
+public class Program_09 {
+    public static void main(String args[]){
+        
+        double dolar    = 3;
+        double qtaDolar = 100;
+        
+        double qtaReais = qtaDolar*dolar;
+        
+        System.out.println("Cotação do Dólar: " + dolar);
+        System.out.println("Quantia em Doláres: " + qtaDolar);
+        System.out.println("Valor convertido (R$): " + qtaReais);
+    }
+}
