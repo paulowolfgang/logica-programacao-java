e801d94944c4f38d570627826aba75d71f044c79
paulo <paulovictor.ps.belem@gmail.com>
2022-11-21 13:27:05 -0300

    :sparkles: feat: Vetor

---
+++ src/Program_32.java
@@ -0,0 +1,15 @@
+public class Program_32
+{
+    public static void main(String argd[]){
+    
+        int []x = new int[5];
+        
+        x[0] = 1;
+        x[1] = 2;
+        x[2] = 3;
+        x[3] = 4;
+        x[4] = 5;
+        
+        System.out.println("Posição N° 01: " + x[0] + "\nPosição N° 05: " + x[4]);
+    }
+}
