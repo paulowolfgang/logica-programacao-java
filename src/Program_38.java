6cd626b6c0a38e8455b529253bb57cfeb939082c
paulo <paulovictor.ps.belem@gmail.com>
2022-11-23 13:21:23 -0300

    :sparkles: feat: Super sena (números randômicos)

---
+++ src/Program_38.java
@@ -0,0 +1,10 @@
+public class Program_38 {
+    public static void main(String args[]){
+    
+        for (int qtd = 1; qtd <= 6; qtd++) {
+            int num = (int) (Math.random() * 60);
+            System.out.println(num + " ");
+        }
+        System.out.println();
+    }
+}
