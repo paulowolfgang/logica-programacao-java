a0b3b96e1c42275c1574935de9ad5416d7954a90
paulo <paulovictor.ps.belem@gmail.com>
2022-12-08 13:26:42 -0300

    :sparkles: feat: Tabuada de 1 à 10

---
+++ src/Program_66.java
@@ -0,0 +1,14 @@
+public class Program_66
+{
+    public static void main(String args[]){
+        
+        int num1 = 1;
+        int i = 1;
+          
+            do{
+                System.out.println(num1 + " x " + i + " = " + (num1 * i));
+                i++;
+            }
+            while(i <= 10);
+    }
+}
